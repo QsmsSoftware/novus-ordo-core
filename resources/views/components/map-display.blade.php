a4b6d2786d478ea285a144152a227eb890d79ee3
fredb <fredb@nospam.com>
2025-11-14 03:35:58 +0000

    Added an optional (CSS) class attribute to x-map-display component.

--- resources/views/components/map-display.blade.php
+++ resources/views/components/map-display.blade.php
@@ -226,7 +226,7 @@ class MapDisplay {
 }
 </script>
 @endonce
-<div id=@json($id)>
+<div class=@json($class??"") id=@json($id)>
     <canvas id=@json($id . "-canvas") width="{{ $map_width_px }}" height="{{ $map_height_px }}"></canvas>
     <div hidden>
         <img id=@json($id . "-map-layer-0") src="res/bundled/map/map_layer_0.png" />
