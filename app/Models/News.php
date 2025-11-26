b059eb38a11545949602f2f7b71230e114b358ac
fredb <fredb@nospam.com>
2025-11-26 22:14:25 +0000

    Added News::create.

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    public static function create(Turn $turn, string $content): News {
+        $news = new News();
+        $news->game_id = $turn->getGame()->getId();
+        $news->turn_id = $turn->getId();
+        $news->content = $content;
+        $news->save();
+
+        return $news;
+    }
 }
